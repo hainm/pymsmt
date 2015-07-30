c28ea45130cdd36104230e6b2f82fa4fe35b726b
Pengfei Li <pengfei@msu.edu>
2015-07-30 15:49:19 -0400

    Fix bug in renaming the residues.

--- mcpb/rename_residues.py
+++ mcpb/rename_residues.py
@@ -14,23 +14,24 @@ def get_diS_bond(mol, atids):
         resids.append(mol.atoms[i].resid)
 
     disul = []
-    #Correct the names of the HIS, ASP, GLU, LYS, CYS
     for i in resids:
-      if mol.residues[i].resname == 'CYM':
-        #for atom in CYM residue
+      if mol.residues[i].resname in ['CYX', 'CYS']:
+        #for atom in CYX or CYS residue
         for j in mol.residues[i].resconter:
           if mol.atoms[j].atname == 'SG':
             sgcrd = mol.atoms[j].crd
+
             #for every atom
             for k in atids:
-              if mol.atoms[k].atname == 'SG' and k != j:
-                atkcrd = mol.atoms[k].crd
-                dis = calc_bond(sgcrd, atkcrd)
-                if dis <= 2.50:
-                  if j < k:
-                    disuls.append((j, k))
-                  else:
-                    disuls.append((k, j))
+              if mol.atoms[k].resname in ['CYX', 'CYS']:
+                if mol.atoms[k].atname == 'SG' and k != j:
+                  atkcrd = mol.atoms[k].crd
+                  dis = calc_bond(sgcrd, atkcrd)
+                  if dis <= 2.50:
+                    if (j < k) and ((j, k) not in disul):
+                      disul.append((j, k))
+                    elif ((k, j) not in disul):
+                      disul.append((k, j))
 
     return disul
 
@@ -44,6 +45,7 @@ def rename_res(mol, atids):
 
     #Correct the names of the HIS, ASP, GLU, LYS, CYS
     for i in resids:
+      #HIS
       if mol.residues[i].resname == 'HIS':
         hasatoms = []
         for j in mol.residues[i].resconter:
@@ -55,6 +57,7 @@ def rename_res(mol, atids):
           mol.residues[i].resname = 'HID'
         elif ('HE2' in hasatoms):
           mol.residues[i].resname = 'HIE'
+      #ASP
       elif mol.residues[i].resname == 'ASP':
         hasatoms = []
         for j in mol.residues[i].resconter:
@@ -62,6 +65,7 @@ def rename_res(mol, atids):
           hasatoms.append(atname)
         if ('HD1' in hasatoms) or ('HD2' in hasatoms):
           mol.residues[i].resname = 'ASH'
+      #GLU
       elif mol.residues[i].resname == 'GLU':
         hasatoms = []
         for j in mol.residues[i].resconter:
@@ -69,6 +73,7 @@ def rename_res(mol, atids):
           hasatoms.append(atname)
         if ('HE1' in hasatoms) or ('HE2' in hasatoms):
           mol.residues[i].resname = 'GLH'
+      #LYS
       elif mol.residues[i].resname == 'LYS':
         hasatoms = []
         for j in mol.residues[i].resconter:
@@ -76,6 +81,7 @@ def rename_res(mol, atids):
           hasatoms.append(atname)
         if ('HZ1' not in hasatoms):
           mol.residues[i].resname = 'LYN'
+      #CYS
       elif mol.residues[i].resname == 'CYS':
         hasatoms = []
         for j in mol.residues[i].resconter:
@@ -92,7 +98,7 @@ def rename_res(mol, atids):
                   atkcrd = mol.atoms[k].crd
                   dis = calc_bond(sgcrd, atkcrd)
                   if dis <= 2.50:
-                    mol.residues[i].resname = 'CYS'
+                    mol.residues[i].resname = 'CYX'
                   else:
                     mol.residues[i].resname = 'CYM'
 
